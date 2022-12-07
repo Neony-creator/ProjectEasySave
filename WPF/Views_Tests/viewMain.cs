02d08e0cded50ece10632c3034c665bca75b7266
ognonZZ <nicolas.pierson@viacesi.Fr>
2022-12-07 15:34:11 +0100

    ajout tests view afin d'enlever les erreurs en attendant de les remplacer

---
+++ WPF/Views_Tests/viewMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetEasySaveCLI
+{
+    class viewMain
+    {
+        public void display(string text) //Fonction qui affiche sur la console les informations envoyées par le controller
+        {
+            Console.Write(text);
+        }
+
+    }
+}
